c6de14c8a42d00b7b206df3e581d52beef03c551
luccasdsm <luccasdsm@hotmail.com>
2023-07-26 21:33:35 -0300

    Remoção do blazorize e adição do mudblazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using WebAdmin.Client;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,11 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services
-    .AddBlazorise(options =>
-    {
-        options.Immediate = true;
-    })
-    .AddBootstrapProviders()
-    .AddFontAwesomeIcons();
+builder.Services.AddMudServices();
+
 await builder.Build().RunAsync();
