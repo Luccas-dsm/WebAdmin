9c240720066139870c6d7003a04f8c3e44b1eacf
luccasdsm <luccasdsm@hotmail.com>
2023-08-02 21:22:19 -0300

    Melhoria do checkin anterior

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using WebAdmin.Client;
-using WebAdmin.Client.Services.Interfaces;
-using WebAdmin.Client.Services.Service;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,7 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
-
-builder.Services.AddSingleton<ICampoService, CampoService>();
 
 await builder.Build().RunAsync();
---
+++ Shared/Enums/Crud/CrudEnum.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace WebAdmin.Shared.Enums
+{
+    public enum CrudEnum : long
+    {
+        [EnumMember]
+        [Description("inserir")]
+        Insert = 1,
+
+        [EnumMember]
+        [Description("atualizar")]
+        Update = 2,
+
+        [EnumMember]
+        [Description("deletar")]
+        Delete = 3
+    }
+}
